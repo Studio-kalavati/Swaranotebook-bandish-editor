694e7b3df40d76d7f13bb52392ee05e0a3dd0499
shark8me <kiran.karkera@gmail.com>
2023-01-11 18:44:45 +0100

    initial commit to html branch

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bandish-community
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/bandish_community/core_test.clj
@@ -0,0 +1,7 @@
+(ns bandish-community.core-test
+  (:require [clojure.test :refer :all]
+            [bandish-community.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
