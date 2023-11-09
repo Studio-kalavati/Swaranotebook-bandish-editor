03fd0d8d2726df2bfd678c244158c9fff0259f9c
shark8me <kiran.karkera@gmail.com>
2023-11-09 23:56:50 +0100

    try to fix #100 again

--- sw-src.js
+++ sw-src.js
@@ -2,21 +2,16 @@ importScripts(
   'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
 
-import {registerRoute} from 'workbox-routing';
-import {StaleWhileRevalidate} from 'workbox-strategies';
-import {precacheAndRoute} from 'workbox-precaching';
-import {NetworkFirst} from 'workbox-strategies';
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
-precacheAndRoute(self.__WB_MANIFEST);
+workbox.routing.registerRoute(/\.(?:png|gif|jpg|jpeg|woff|woff2|eot|ttf|svg)$/, new CacheFirst());
 
-registerRoute(/\.(?:png|gif|jpg|jpeg|woff|woff2|eot|ttf|svg)$/, new CacheFirst());
-
-registerRoute(
+workbox.routing.registerRoute(
   ({url}) => url.pathname.startsWith('/view/'),
-  new NetworkFirst()
+  workbox.strategies.networkFirst()
 );
 
-registerRoute(
+workbox.routing.registerRoute(
   /\.(?:js|html|css)$/,
-  new StaleWhileRevalidate()
+  workbox.strategies.staleWhileRevalidate()
 );
