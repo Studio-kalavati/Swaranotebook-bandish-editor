52ac56ea00ad1c83152f773b2ff9c3448cd28103
shark8me <kiran.karkera@gmail.com>
2025-05-10 01:07:05 +0200

    chore: update karma configuration for Puppeteer and remove JUnit reporting

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,19 @@
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 module.exports = function (config) {
-  var junitOutputDir = process.env.CIRCLE_TEST_REPORTS || "target/junit"
 
   config.set({
     browsers: ['ChromeHeadless'],
-    basePath: 'target',
-    files: ['karma-test.js'],
+    basePath: 'out',
+      files: ['puppeteer-tests.js'],
     frameworks: ['cljs-test'],
     plugins: [
         'karma-cljs-test',
-        'karma-chrome-launcher',
-        'karma-junit-reporter'
-    ],
+        'karma-chrome-launcher'],
     colors: true,
     logLevel: config.LOG_INFO,
     client: {
       args: ['shadow.test.karma.init']
     },
 
-    // the default configuration
-    junitReporter: {
-      outputDir: junitOutputDir + '/karma', // results will be saved as outputDir/browserName.xml
-      outputFile: undefined, // if included, results will be saved as outputDir/browserName/outputFile
-      suite: '' // suite will become the package name attribute in xml testsuite element
-    }
   })
 }
